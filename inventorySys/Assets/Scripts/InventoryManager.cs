5f5d0c18ed6feb37d2d86cc437bb90b2dbc4e05b
LoriaRujoy <1306650725@qq.com>
2025-02-16 20:43:35 +0800

    更新物品的Json文件， Resource文件夹改为Resources； 天啊及JsonObject插件；

--- inventorySys/Assets/Scripts/InventoryManager.cs
+++ inventorySys/Assets/Scripts/InventoryManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 /// </summary>
 public class InventoryManager : MonoBehaviour
 {
+    #region 单例模式
     private static InventoryManager _instance;
     public static InventoryManager Instance
     {
@@ -18,5 +19,36 @@ public class InventoryManager : MonoBehaviour
             }
             return _instance;
         }
+    }
+    #endregion
+    
+    public List<Item> itemList= new List<Item>();//物品列表
+
+    private void Start()
+    {
+        ParseItemJson();
+    }
+
+    /// <summary>
+    /// 解析物品json
+    /// </summary>
+    void ParseItemJson()
+    {
+        try
+        {
+            TextAsset itemText = Resources.Load<TextAsset>("Items");
+            if (itemText == null)
+            {
+                Debug.LogError("未能找到Items资源文件");
+                return;
+            }
+            string itemJson = itemText.text;
+            Debug.Log(itemJson);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"解析发生错误：{e.Message}");
+        }
+        
     }
 }
